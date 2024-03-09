3efed5e6510ce425cfc11473949a06016669d505
sonamguptacs <sonamcs3092@gmail.com>
2024-03-09 22:21:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+ 
+    
+      const heading = React.createElement("h1", {}, "Sample React Code without npx");
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(heading);
+    
