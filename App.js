adfcc45193e63035b03675c7228cf74aaa68962b
sonamguptacs <sonamcs3092@gmail.com>
2024-03-10 01:28:41 +0530

    installed parcel and react

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
  
-    
-      const heading = React.createElement("h1", {}, "Sample React Code without npx");
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(heading);
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const heading = React.createElement("h1", {}, "Sample React Code without npx");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
     
---
+++ Readme.md
@@ -0,0 +1,11 @@
+#Learnt about
+
+  - Parcel
+  - Browserslists
+  - Difference between dev and production code
+  - Difference between ^ and ~
+  - Difference between package and package.json
+  - Creating React app without create-react-app
+  - Disadvantage of using cdn
+  - How is our code fast?
+  
