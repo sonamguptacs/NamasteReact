50ef6ad544f50621c00373d269621855108c1189
sonamguptacs <sonamcs3092@gmail.com>
2024-03-14 00:07:31 +0530

    JSX , React Element , React Component

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 // React.createElement => object => Rendered
-// const heading = React.createElement("h1", {}, "Sample React Code without npx");
+const heading = React.createElement("h1", {}, "Sample React Code without npx");
 
 /**JSX is not React
  * 
  * JSX => Reat.createElement => object => Rendered
 */
 
-const heading =<h1>Hello</h1>
+//React Element
+const title = <h1>Hello</h1>
+
+//React Functional Component
+const Heading=()=>title
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+root.render(<Heading/>);
     
